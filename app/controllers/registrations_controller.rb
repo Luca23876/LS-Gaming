b2d63849e7e2e738ffdd24382fd404fbc06bd3d6
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2016-09-22 17:01:11 -0600

    sign up fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,8 +9,9 @@ class RegistrationsController < ApplicationController
 :password_confirmation))
 	if @user.save
 		session[:user_id] = @user.id
-		redirect_to index
+		redirect_to mainpage_path
 	else
+		@sign_in_error
 	   render :new
 	end
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 root "authentication#new"
-get "welcome/games" => "welcome#games"
-get "welcome/index" => "welcome#index", as: :index
-get "welcome/createanaccount" => "welcome#createanaccount"
+#get "welcome/games" => "welcome#games"
+get '/welcome' => 'welcome#index', as: :mainpage
+#get "welcome/createanaccount" => "welcome#createanaccount"
 get '/sign-up' => 'registrations#new', as: :signup
 post '/sign-up' => 'registrations#create'
 get '/sign-in' => 'authentication#new', as: :signin
