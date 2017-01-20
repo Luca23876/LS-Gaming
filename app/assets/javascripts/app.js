0380e697187f8683cd2f2f34928cea08af9ac537
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2017-01-19 18:01:42 -0700

    updated  html files/ fixed navbar appearing in signin error

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,18 +1,10 @@
-$(document).ready(function() {
-
-	$('form').on('submit', function(e) {
-		e.preventDefault();
-		var lucasCoolInputValue=document.getElementById('lucas-input-field').value;
-		console.log(lucasCoolInputValue);
-  	   	//now we want to print the information on to the page
-  	   	document.getElementById("password-box-printout").innerHTML = lucasCoolInputValue;
-      function myfunction(){
-        var x = document.getElementById("myTopnav")
-        if (x.className === "Topnav"){
-          x.className += " responsive"
-        } else {
-          x.className = "topnav";
-        }
-      }
-  });
-});  
+// $(document).ready(function() {
+//   function myfunction(){
+//     var x = document.getElementById("myTopnav")
+//     if (x.className === "Topnav"){
+//       x.className += " responsive"
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+// });  
